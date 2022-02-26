bcda5b0d63f7ad86b44e53c2f29bdbdf238c3026
Berdin7941VC <Goofy_01@mail.ru>
2022-02-26 12:30:32 +0300

    Alpha and Blend tests ready, Scissor test doesn't work

--- lab1/glscene.h
+++ lab1/glscene.h
@@ -2,22 +2,43 @@
 #define GLSCENE_H
 
 #include <QOpenGLWidget>
-#include <QOpenGLFunctions>
 #include <iostream>
+#include <string>
 
-class glScene : public QOpenGLWidget, public QOpenGLFunctions
+class glScene : public QOpenGLWidget
 {
     Q_OBJECT
-    int GLType;
+    GLenum GLType;
+    GLenum alphaFunction;
+    float alphaRefValue;
+    GLenum sfactor;
+    GLenum dfactor;
+    float xLb, yLb;
+    float heightLb, widthLb;
+    bool alphaFlag;
+    bool scissorFlag;
+    bool blendFlag;
+
 public:
     glScene(QWidget *parent = nullptr);
 
     void initializeGL() override;
     void resizeGL(int w, int h) override;
     void paintGL() override;
+    void drawTriangles();
+    void drawLines();
+    void setVariable();
 
 public slots:
     void slot(int);
+    void alphaChooseSlot(int);
+    void alphaRefSlot(int);
+    void sfactorSlot(int);
+    void dfactorSlot(int);
+    void xSlot(int);
+    void ySlot(int);
+    void heightSlot(int);
+    void widthSlot(int);
 };
 
 #endif // GLSCENE_H
--- lab1/mainwindow.cpp
+++ lab1/mainwindow.cpp
@@ -7,6 +7,15 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     connect(ui->comboBox_lab_1, &QComboBox::activated, ui->openGL_main, &glScene::slot);
+    connect(ui->comboBox_transparency, &QComboBox::activated, ui->openGL_main, &glScene::alphaChooseSlot);
+    connect(ui->horizontalSlider_transparency, &QSlider::valueChanged, ui->openGL_main, &glScene::alphaRefSlot);
+    connect(ui->horizontalSlider_x, &QSlider::valueChanged, ui->openGL_main, &glScene::xSlot);
+    connect(ui->horizontalSlider_y, &QSlider::valueChanged, ui->openGL_main, &glScene::ySlot);
+    connect(ui->horizontalSlider_height, &QSlider::valueChanged, ui->openGL_main, &glScene::heightSlot);
+    connect(ui->horizontalSlider_width, &QSlider::valueChanged, ui->openGL_main, &glScene::widthSlot);
+    connect(ui->comboBox_mixing_1, &QComboBox::activated, ui->openGL_main, &glScene::sfactorSlot);
+    connect(ui->comboBox_mixing_2, &QComboBox::activated, ui->openGL_main, &glScene::dfactorSlot);
+
 }
 
 MainWindow::~MainWindow()
