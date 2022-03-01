d752edda6bc0ff6fb1b24c4ace69e2d991b128b8
Berdin7941VC <Goofy_01@mail.ru>
2022-03-01 06:46:19 +0300

    Micro changes

--- lab1/glscene.h
+++ lab1/glscene.h
@@ -2,43 +2,22 @@
 #define GLSCENE_H
 
 #include <QOpenGLWidget>
+#include <QOpenGLFunctions>
 #include <iostream>
-#include <string>
 
-class glScene : public QOpenGLWidget
+class glScene : public QOpenGLWidget, public QOpenGLFunctions
 {
     Q_OBJECT
-    GLenum GLType;
-    GLenum alphaFunction;
-    float alphaRefValue;
-    GLenum sfactor;
-    GLenum dfactor;
-    float xLb, yLb;
-    float heightLb, widthLb;
-    bool alphaFlag;
-    bool scissorFlag;
-    bool blendFlag;
-
+    int GLType;
 public:
     glScene(QWidget *parent = nullptr);
 
     void initializeGL() override;
     void resizeGL(int w, int h) override;
     void paintGL() override;
-    void drawTriangles();
-    void drawLines();
-    void setVariable();
 
 public slots:
     void slot(int);
-    void alphaChooseSlot(int);
-    void alphaRefSlot(int);
-    void sfactorSlot(int);
-    void dfactorSlot(int);
-    void xSlot(int);
-    void ySlot(int);
-    void heightSlot(int);
-    void widthSlot(int);
 };
 
 #endif // GLSCENE_H
